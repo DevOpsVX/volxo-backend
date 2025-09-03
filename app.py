52d2be55fef74bd8a758aa43d10338d1c2e2fa46
DevOpsVX <luana@volxo.com.br>
2025-09-03 14:20:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-FRONT_URL = "https://volxo-ad-insight.onrender.com"
+FRONT_URL = "https://volxo-ad-insight.onrender.com"  # seu domínio do front
 CORS(app, resources={r"/api/*": {"origins": FRONT_URL}})
 
 @app.post("/api/generate-report")
@@ -10,5 +10,4 @@ def generate_report():
     channels = request.form.get("channels", "")
     custom = request.form.get("customInstructions")
     files = request.files.getlist("files")
-    # TODO: processe e gere o conteúdo
     return jsonify({"content": "Seu relatório gerado aqui..."})
